748e4099cb32a52b85153aef89ec10cef02f18ce
Michael Ruggiero <python@michael.ruggiero.co>
2018-04-13 15:06:42 -0400

    Add AirPrint function (untested)
    Add airprint function found online. Not tested yet.

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Pythonista Community Modules - A curated set of helpful modules to assist in developing on the Pythonista Platform.
+
+Modules are collected from various sources, when known, the author and reference URL is provided.
+
+In additon to the community modules, I have developed a series of modules which I have created for my own use.
+
+To add:
+    * https://github.com/tdamdouni/Pythonista/blob/master/_2017/SearchableTable.py
+
+To do:
+    * Tidy / 
+"""
+
+from utility import airprint
+
+
+__all__ = ['airprint']
